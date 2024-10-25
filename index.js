e845029bef37e27404c6f1d2d1ae2b13b63b0cb9
unknown <conquynho.hd@gmail.com>
2024-10-25 23:01:12 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD", Date.now());
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+core.setOutput("time", time);
+
+const payload = JSON.stringify(github.context.payload);
+
+console.log(`The event payload: ${payload}`);
