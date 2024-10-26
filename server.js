996c084afd5d6b4ea9b0d95729eb18f553823e30
unknown <conquynho.hd@gmail.com>
2024-10-26 15:34:27 +0700

    create express for receive payload

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-import SmeeClient from "smee-client";
+import express from "express";
 
-import "dotenv/config";
+// This initializes a new Express application.
+const app = express();
 
-const smee = new SmeeClient({
-  source: process.env.WEBHOOK_PROXY_URL,
-  target: "http://localhost:3000/webhook",
-  logger: console,
+//
+app.post("/webhook", express.json({ type: "application/json" }), (request, response) => {
+  response.status(202).send("Accepted");
+
+  console.log("Header", request.header);
+  console.log("Body", request.body);
+
+  // Check the `x-github-event` header to learn what event type was sent.
+  const githubEvent = request.headers["x-github-event"];
+
+  // if (githubEvent === "issues") {
+  //   const data = request.body;
+  //   const action = data.action;
+  //   if (action === "opened") {
+  //     console.log(`An issue was opened with this title: ${data.issue.title}`);
+  //   } else if (action === "closed") {
+  //     console.log(`An issue was closed by ${data.issue.user.login}`);
+  //   } else {
+  //     console.log(`Unhandled action for the issue event: ${action}`);
+  //   }
+  // } else if (githubEvent === "ping") {
+  //   console.log("GitHub sent the ping event");
+  // } else {
+  //   console.log(`Unhandled event: ${githubEvent}`);
+  // }
 });
 
-const events = smee.start();
+const port = 3000;
 
-// process.on("exit", () => {
-//   console.log("EXIT");
-//   events.close();
-// });
-
-process.on("SIGINT", () => {
-  console.log("SIGINT");
-  events.close();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-// Stop forwarding events
