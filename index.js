b0f57716055100cef1cff3a32abf3fc2164c526a
unknown <conquynho.hd@gmail.com>
2024-10-26 12:00:30 +0700

    test output

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 export default async ({ core, context, github }) => {
-  core.setOutput("time", Date.now());
+  core.setOutput("time2", Date.now());
 
   console.log(JSON.stringify(github));
   console.log(JSON.stringify(context));
